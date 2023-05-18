21de58e6015f78cd339350945db47207f7b0e0c5
Berkant Baran Subaşı <berkant_baran13@hotmail.com>
2023-05-18 15:15:38 +0300

    setting db connection

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,17 +19,32 @@ namespace PlatformServices
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env=env;
         }
 
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt=>
-            opt.UseInMemoryDatabase("InMem"));
+            if(_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SqlServer DB");
+                services.AddDbContext<AppDbContext>(opt=>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")) );
+
+            }
+            else{
+                    Console.WriteLine("--> Using InMem DB");
+                    services.AddDbContext<AppDbContext>(opt=>
+                    opt.UseInMemoryDatabase("InMem"));
+            }
+          
             services.AddScoped<IPlatformRepo,PlatformRepo>();
             services.AddHttpClient<ICommandDataClient,HttpComandDataClient>();
             services.AddControllers();
