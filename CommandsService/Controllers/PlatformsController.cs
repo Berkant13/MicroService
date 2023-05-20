8d5338a84327d65b4d8e8e6cbe397a64b05ccf6d
Berkant Baran Subaşı <berkant_baran13@hotmail.com>
2023-05-20 21:21:18 +0300

    commit

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -7,9 +12,20 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController:ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository=repository;
+            _mapper=mapper;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("------->Getting Platforms from CommandService");
+            var platformItems=_repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
         [HttpPost]
         public ActionResult TestInBoundConnection()
